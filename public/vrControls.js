110f6929c9313da6da0964ef14f042b50e4ae61b
Mike <mkoval3@gatech.edu>
2017-11-22 14:41:59 -0500

    added gamma calibration

--- public/vrControls.js
+++ public/vrControls.js
@@ -455,10 +455,10 @@ var DeviceOrientationController = function ( object, domElement ) {
 
 					console.log(gamma - this.startGamma);
 					if(this.startOrient > 0){
-						deviceQuat = createQuaternion( 3.14159 / 2 + (alpha - this.startAlpha ) ,  beta, gamma - this.startGamma, orient );
+						deviceQuat = createQuaternion( 3.14159 / 2 + (alpha - this.startAlpha ) ,  beta, 3.14159 / 2 +  gamma - this.startGamma, orient );
 					}
 					else {
-						deviceQuat = createQuaternion( -3.14159 / 2 + (alpha - this.startAlpha ) ,  beta, gamma-this.startGamma, orient );
+						deviceQuat = createQuaternion( -3.14159 / 2 + (alpha - this.startAlpha ) ,  beta, 3.14159 / 2 + gamma-this.startGamma, orient );
 					}
 					
 
