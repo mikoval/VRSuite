d66e84138678d84dffa073e02fa8c1218260691a
Mike <mkoval3@gatech.edu>
2017-12-03 22:02:02 -0500

    reduced texture quality

--- public/ballcollisions/floor.js
+++ public/ballcollisions/floor.js
@@ -9,8 +9,8 @@ function Floor(res, pathsize, type){
     fluidWidth =res
     fluidHeight = res
 	
-	this.densityA = new THREE.WebGLRenderTarget( fluidWidth , fluidHeight, {type: THREE.FloatType,});
-	this.densityB = new THREE.WebGLRenderTarget( fluidWidth , fluidHeight, {type: THREE.FloatType} );
+	this.densityA = new THREE.WebGLRenderTarget( fluidWidth , fluidHeight, {type: THREE.UnsignedByteType,});
+	this.densityB = new THREE.WebGLRenderTarget( fluidWidth , fluidHeight, {type: THREE.UnsignedByteType} );
 	var geometry = new THREE.PlaneGeometry( 50, 50 );
 	var material = new THREE.MeshPhongMaterial({map: this.densityA});
 
